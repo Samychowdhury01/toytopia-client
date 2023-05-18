0f46c4a35500380b7071fe31cf99935e38541fd8
Samychowdhury01 <samychowdhury84@gmail.com>
2023-05-19 01:14:02 +0600

    implemented search functionality in all toys page

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -6,6 +6,14 @@ const AllToys = () => {
   const loadedToys = useLoaderData()
   const [toys, setToys] =useState(loadedToys)
 
+const handleSearch = event =>{
+  event.preventDefault()
+  const searchText = event.target.search.value
+ 
+  fetch(`http://localhost:5000/searchToyByName/${searchText}`)
+  .then(res => res.json())
+  .then(data => setToys(data))
+}
 
   return (
     <div>
@@ -14,7 +22,33 @@ const AllToys = () => {
       </div>
       <div>
       <div className="flex justify-center mb-10">
-      
+      <form onSubmit={handleSearch} className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              name="search"
+              placeholder="Search Toy By Name…"
+              className="input input-bordered"
+            />
+            <button className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+            <p className="text-center mt-5">{toys.length === 0 ? 'No Data Found' : ''}</p>
+        </form>
       </div>
         <div className="overflow-x-auto">
           <table className="table w-full">
@@ -32,7 +66,7 @@ const AllToys = () => {
             </thead>
             <tbody>
              {
-                toys.map((toy, index) =>
+                toys?.map((toy, index) =>
                   <ToyRow
                   key={toy?._id}
                   index={index}
