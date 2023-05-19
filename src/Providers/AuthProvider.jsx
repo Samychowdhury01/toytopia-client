6abe9557b0ad9b5007b7438d0b62f95f392296ca
Samychowdhury01 <samychowdhury84@gmail.com>
2023-05-19 14:45:45 +0600

    auth provider setup complete.

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,19 +1,74 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
 
-const AuthProvider = ({children}) => {
+const auth = getAuth(app);
 
+// google auth provider
+const googleProvider = new GoogleAuthProvider();
 
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-    const authInfo = {
+  // sign in with google 
+  const googleSignIn = () =>{
+    setLoading(true)
+    return signInWithPopup(auth, googleProvider)
+  }
 
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // create user with email and password 
+const createUser = (email, password) =>{
+  setLoading(true)
+return createUserWithEmailAndPassword(auth, email, password)
+}
+
+// login user with email and password 
+const loginWithEmil = (email, password) =>{
+  setLoading(true)
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+// Update photo and name
+const updateUserProfile=(name,photoUrl)=>{
+  return updateProfile(auth.currentUser,{
+      displayName:name,
+      photoURL:photoUrl
+  })
+}
+
+// sign-out user
+const logOut = () => {
+  return signOut(auth);
+};
+
+// observer 
+useEffect(() => {
+  const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+    setLoading(false);
+  });
+  return () => {
+    unsubscribe();
+  };
+}, []);
+
+  const authInfo = {
+    user,
+    loading,
+    googleSignIn,
+    createUser,
+    loginWithEmil,
+    updateUserProfile,
+    logOut
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+      </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
